255d1dc354593dd6fa5d0f43ac02a06838e32add
Juha Autero <jautero@iki.fi>
2016-05-14 16:12:07 +0300

    First version of bot.

---
+++ HopperBot.py
@@ -0,0 +1,5 @@
+class HopperBot:
+    def __init__(self):
+        pass
+    def process(self,msg):
+        return "A likely story!"
---
+++ HopperBot_test.py
@@ -0,0 +1,14 @@
+import unittest
+import HopperBot
+
+class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.bot=HopperBot.HopperBot()
+        self.responses=["I don't see what that has to do with anything!","A likely story!"]
+        
+    def test_initialization(self):
+        assert self.bot != None
+
+    def test_message_processing(self):
+        response=self.bot.process("Hello World!")
+        assert response in self.responses
