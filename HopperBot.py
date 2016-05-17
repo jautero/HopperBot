8328dce437412ac10aa72f94b339210b8d5e0f0a
Juha Autero <jautero@iki.fi>
2016-05-17 22:55:21 +0300

    Completed pinky.

--- HopperBot.py
+++ HopperBot.py
@@ -1,9 +1,13 @@
-import random
+import random, json
 
 class HopperBot:
-    def __init__(self):
+    def __init__(self,name):
         self.stockanswers=["A likely story!","I don't see what that has to do with anything!"]
-    def process(self,msg):
-        if msg == "Are you pondering what I'm pondering?":
-            return "Poit, I guess I am."
+        self.name=name
+        pinkydata=json.load(file("pinky.json"))
+        self.pinkyquestions=set(q.format(name) for q in pinkydata["questions"])
+        self.pinkyanswers=set(pinkydata["answers"])
+    def process(self,nick,msg):
+        if msg in self.pinkyquestions:
+            return random.choice(list(self.pinkyanswers)).format(nick)
         return random.choice(self.stockanswers)
--- HopperBot_test.py
+++ HopperBot_test.py
@@ -1,18 +1,24 @@
-import unittest
+import unittest, json
 import HopperBot
 
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
-        self.bot=HopperBot.HopperBot()
+        self.nick="tester"
+        self.botname="testbot"
+        self.bot=HopperBot.HopperBot(self.botname)
+        pinkydict=json.load(file("pinky.json"))
         self.responses=["I don't see what that has to do with anything!","A likely story!"]
+        self.pinkyanswers=[a.format(self.nick) for a in pinkydict["answers"]]
+        self.pinkyquestions=pinkydict["questions"]
         
     def test_initialization(self):
         assert self.bot != None
 
     def test_message_processing(self):
-        response=self.bot.process("Hello World!")
+        response=self.bot.process(self.nick,"Hello World!")
         assert response in self.responses
         
     def test_pinky(self):
-        response=self.bot.process("Are you pondering what I'm pondering?")
-        assert response == "Poit, I guess I am."
+        for question in self.pinkyquestions:
+            answer=self.bot.process(self.nick,question.format(self.botname))
+            assert answer in self.pinkyanswers
