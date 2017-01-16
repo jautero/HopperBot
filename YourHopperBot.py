f85fd0d045a5160ef82c73bd74fbc3ffd254161b
Juha Autero <jautero@iki.fi>
2017-01-16 11:20:46 +0200

    Added yaml config file.

--- YourHopperBot.py
+++ YourHopperBot.py
@@ -2,8 +2,11 @@
 #This is your bot configuration.
 import HopperBot
 import TelegramAPI
+import os, yaml
+
+config=yaml.load(file(os.path.expanduser(os.path.join("~",".HopperBot.yaml"))))
 
 bot=HopperBot.HopperBot()
-api=TelegramAPI.API(bot,"APIKEY")
+api=TelegramAPI.API(bot,config.telegram.apikey)
 while True:
     pass
