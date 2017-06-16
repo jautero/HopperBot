de829e60ee2add2f2037c8b90378d066c866e158
Juha Autero <jautero@iki.fi>
2017-06-16 20:27:17 +0300

    Updated to use python-telegram-bot

--- HopperBot.py
+++ HopperBot.py
@@ -1,35 +1,20 @@
-import random, json, re, KeywordStore, StringIO
+from telegram.ext import Updater, CommandHandler
+import yaml, os
 import logging
+
+def handle_user_id(bot, update):
+    update.message.reply_text(
+        "You are {}".format(update.message.from_user.id))
+
+def register_handlers(updater):
+    updater.dispatcher.add_handler(CommandHandler("id",handle_user_id))
+
 logging.basicConfig(filename="test.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-class HopperBot:
-    def __init__(self,name):
-        self.stockanswers=["A likely story!","I don't see what that has to do with anything!"]
-        self.name=name
-        self.memory=KeywordStore.KeywordStore()
-        pinkydata=json.load(file("pinky.json"))
-        self.pinkyquestions=set(q.format(name) for q in pinkydata["questions"])
-        self.pinkyanswers=set(pinkydata["answers"])
-    def process(self,msg):
-        if msg.text == self.name + "!":
-            return msg.name + "!"
-        if msg.text == "dump memory":
-            return self.dump_memory()
-        if msg.text == "id?":
-            return msg.userid
-        if msg.text in self.pinkyquestions:
-            return random.choice(list(self.pinkyanswers)).format(msg.name)
-        try:
-            return self.recall(re.sub('\W',' ',msg.text).lower().split()).pop()
-        except IndexError:
-            self.remember(msg.text)
-            return random.choice(self.stockanswers)
-    def remember(self,msg):
-        tokens=re.sub('\W',' ',msg).lower().split()
-        self.memory.store(msg,tokens)
-    def recall(self,tokens):
-        return self.memory.get(tokens)
-    def dump_memory(self):
-        stream=StringIO.StringIO()
-        self.memory.dump(stream)
-        return stream.getvalue()
+if __name__ == '__main__':
+    config=yaml.load(file(os.path.expanduser(os.path.join("~",".HopperBot.yaml"))))
+    updater=Updater(config["telegram"]["apikey"])
+
+    register_handlers(updater)
+    updater.start_polling()
+    updater.idle()
