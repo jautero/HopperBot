d8f78500d0e70f71221007e925b093e4fca6463a
Juha Autero <jautero@iki.fi>
2017-06-17 02:08:28 +0300

    Updated setup for chatterbot

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name="HopperBot",
-      version="0.3",
-      description="HopperBot is a chatbot that can be programmed with simple sentences. It is named after Admiral Grace Hopper.",
+      version="0.4",
+      description="HopperBot is a chatbot that learns. It is named after Admiral Grace Hopper.",
       author="Juha Autero",
       author_email="jautero@iki.fi",
       url="https://github.com/jautero/HopperBot",
-      install_requires=["pytest","python-telegram-bot","pyyaml"],
+      install_requires=["pytest","python-telegram-bot","pyyaml","chatterbot"],
       py_modules = ['HopperBot'])
