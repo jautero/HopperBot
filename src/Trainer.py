8b6fc4e2357c4e2472b743b16164208b64806c68
Juha Autero <jautero@iki.fi>
2019-02-12 17:52:39 +0200

    Fixed config file path.

--- src/Trainer.py
+++ src/Trainer.py
@@ -2,7 +2,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import yaml,os
 
-config=yaml.load(open(os.path.expanduser(os.path.join("~",".HopperBot.yaml"))))
+config=yaml.load(open(os.path.expanduser(os.path.join("/app/config","HopperBot.yaml"))))
 
 chatbot=ChatBot('Trainer',
     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
