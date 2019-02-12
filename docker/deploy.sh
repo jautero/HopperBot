836f361d20b2c1cd6689638b510c9d9eda2fbcef
Juha Autero <jautero@iki.fi>
2019-02-12 18:04:57 +0200

    Added missing network.

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,4 +3,4 @@ cd /srv/HopperBot
 CONTAINER_ID=`cat container.id`
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
-docker run -d -v $PWD/config:/app/config jautero/hopperbot python HopperBot.py >container.id
+docker run -d -v $PWD/config:/app/config jautero/hopperbot --network web python HopperBot.py >container.id
