58bca4e0412f09179a59cf57fc8834ee3e3a24ec
Juha Autero <jautero@iki.fi>
2019-03-12 05:17:42 +0200

    Added $WORKSPACE to config file path.

--- test/test_image_store.py
+++ test/test_image_store.py
@@ -1,7 +1,7 @@
-import unittest, yaml
+import unittest, yaml, os
 from image_store import ImageStore
 
-config=yaml.load(open("test/test_config.yaml"))
+config=yaml.load(open(os.path.expandvars("$WORKSPACE/test/test_config.yaml")))
 
 class TestImageStore(unittest.TestCase):
     def setUp(self):
