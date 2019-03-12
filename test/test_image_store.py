ae491ae60b5c7c84fd1b745ef7ef39a98591d293
Juha Autero <jautero@iki.fi>
2019-03-12 04:40:39 +0200

    Added assert for key and secret.

--- test/test_image_store.py
+++ test/test_image_store.py
@@ -5,3 +5,5 @@ from image_store import ImageStore
 class TestImageStore(unittest.TestCase):
     def test_image_store(self):
         dut=ImageStore("foo","bar")
+        self.assertEqual(dut.key,"foo")
+        self.assertEqual(dut.secret,"bar")
