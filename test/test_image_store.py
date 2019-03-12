3fdbebc89f6c0d2716dbc7b7285cac7f3f5cbd85
Juha Autero <jautero@iki.fi>
2019-03-12 04:30:01 +0200

    Refactored import  out of test.

--- test/test_image_store.py
+++ test/test_image_store.py
@@ -1,6 +1,7 @@
 import unittest
+from image_store import ImageStore
+
 
 class TestImageStore(unittest.TestCase):
     def test_image_store(self):
-        from image_store import ImageStore
         dut=ImageStore("foo","bar")
