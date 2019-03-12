b0d0af3e7f015c82da3a2a36603a19251226d165
Juha Autero <jautero@iki.fi>
2019-03-12 04:48:34 +0200

    Refactored secret and key in test setup.

--- test/test_image_store.py
+++ test/test_image_store.py
@@ -3,8 +3,9 @@ from image_store import ImageStore
 
 
 class TestImageStore(unittest.TestCase):
-    secret="bar"
-    key="foo"
+    def setUp(self):
+        self.secret="bar"
+        self.key="foo"
     def test_image_store(self):
         dut=ImageStore(self.key,self.secret)
         self.assertEqual(dut.key,self.key)
