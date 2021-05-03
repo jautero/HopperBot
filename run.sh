9ac48d654326cc49a3836b5e18e1b3ec567de6c1
Juha Autero <jautero@iki.fi>
2021-05-04 00:34:20 +0300

    Moved settings to home directory.

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-domain=jautero.ems.host
-export MATRIX_USER="@hopperbot:$domain"
-export MATRIX_ACCESS_TOKEN="MDAxZWxvY2F0aW9uIGphdXRlcm8uZW1zLmhvc3QKMDAxM2lkZW50aWZpZXIga2V5CjAwMTBjaWQgZ2VuID0gMQowMDJlY2lkIHVzZXJfaWQgPSBAaG9wcGVyYm90OmphdXRlcm8uZW1zLmhvc3QKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMjFjaWQgbm9uY2UgPSBGNz1wV1B0LGFER0VjdmErCjAwMmZzaWduYXR1cmUg2Zw2wSmY4bPE3_UI_GBCxl38gFOP06--nuErlHOvre8K" 
-export MATRIX_SERVER="https://$domain"
-# export JOIN_ON_INVITE="True"
-export BOT_OWNERS="@jautero:$domain"
+. $HOME/.jautero.ems.host.env
+git pull 
 pipenv run python3 bot.py
