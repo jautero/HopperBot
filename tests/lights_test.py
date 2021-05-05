3b1a084d6f38b52767099d5ffac2a8b99e8be1a1
Juha Autero <jautero@iki.fi>
2021-05-06 01:02:05 +0300

    Created fixture for module and 2 simple tests.

--- tests/lights_test.py
+++ tests/lights_test.py
@@ -1,5 +1,14 @@
+import pytest
 import modules.lights
+from modules.common.module import BotModule
 
-def test_module():
-  dot=lights.LightsModule()
-  assert lights
+@pytest.fixture
+def dut():
+  return modules.lights.LightsModule("lights")
+
+def test_create_module(dut):
+  assert dut
+
+def test_is_botmodule(dut):
+  assert isinstance(dut,BotModule)
+  
