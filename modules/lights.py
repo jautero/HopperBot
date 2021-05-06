86a87babfa6d599056d1f7208b89308c8ef840e4
Juha Autero <jautero@iki.fi>
2021-05-07 00:06:33 +0300

    Added mocked asyncio test

--- modules/lights.py
+++ modules/lights.py
@@ -1,3 +1,4 @@
+import aiohttp, os
 from modules.common.module import BotModule
 
 class LightsModule(BotModule):
@@ -7,4 +8,11 @@ class LightsModule(BotModule):
 
   def help(self):
     return 'Control lights'
-  
+
+  async def lights_on(self,entity_id):
+    instance=os.environ['HA_URL']
+    token=os.environ["HA_TOKEN"]
+    headers={'Authorization': 'Bearer '+token,}
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.post(instance+'/api/services/light/turn_on',json={"entity_id":entity_id}) as resp:
+          return resp.status
--- tests/lights_test.py
+++ tests/lights_test.py
@@ -1,4 +1,6 @@
 import pytest
+from aioresponses import aioresponses
+
 import modules.lights
 from modules.common.module import BotModule
 
@@ -6,9 +8,18 @@ from modules.common.module import BotModule
 def dut():
   return modules.lights.LightsModule("lights")
 
+@pytest.fixture
+def mock_aiohttpclient():
+  with aioresponses() as mock_client:
+    yield mock_client
+
 def test_create_module(dut):
   assert dut
 
 def test_is_botmodule(dut):
   assert isinstance(dut,BotModule)
-  
+
+@pytest.mark.asyncio
+async def test_lights_on(dut,mock_aiohttpclient):
+  mock_aiohttpclient.post('https://za6qz3v9vsv57ez6xmh5ubphzkys1xky.ui.nabu.casa/api/services/light/turn_on',status=200)
+  assert 200 == await dut.lights_on("light.eteisen_kattolamppu")
